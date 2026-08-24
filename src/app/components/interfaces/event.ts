20d0193aeb3ab0f8b4f6f4f9c743147b6ccda7f0
Bryant2498 <aldin5758@gmail.com>
2026-08-24 11:38:46 +0700

    opsi ticket

--- src/app/components/interfaces/event.ts
+++ src/app/components/interfaces/event.ts
@@ -1,11 +1,25 @@
 import { Timestamp } from "firebase/firestore";
 
+export interface TicketPackageInterface {
+  id: string;
+  name: string;
+  price: number;
+  badge?: string;
+  badgeBg?: string;
+  badgeText?: string;
+  description?: string;
+}
+
 export interface EventInterface {
   id: string;
   description: string;
   isSoldOut: boolean;
   location: string;
   price: number;
+  price_festival?: number;
+  price_vip?: number;
+  packages?: TicketPackageInterface[];
+  ticket_types?: TicketPackageInterface[];
   src: string;
   sub_title: string;
   ticket: number;
